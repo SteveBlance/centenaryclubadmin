3c221db9a822cbb943c67bf47a9c4f1dfab42858
SteveBlance <stephen.blance@gmail.com>
2025-02-26 22:17:19 +0000

    Changes to resolve issues found when deploying in AWS

--- src/main/java/com/codaconsultancy/centenaryclubadmin/CentenaryClubAdminApplication.java
+++ src/main/java/com/codaconsultancy/centenaryclubadmin/CentenaryClubAdminApplication.java
@@ -2,8 +2,16 @@ package com.codaconsultancy.centenaryclubadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@ComponentScan({"com.codaconsultancy"})
+@EntityScan("com.codaconsultancy.centenaryclubadmin.domain")
+@EnableJpaRepositories("com.codaconsultancy.centenaryclubadmin.repositories")
+@EnableScheduling
 public class CentenaryClubAdminApplication {
 
 	public static void main(String[] args) {
